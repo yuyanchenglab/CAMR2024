543c937823fb1f8d146e67ae238fd7b10e451f6a
Albert Chen <chena23@node153.hpc.local>
2024-06-11 12:57:33 -0400

    Added ambient DNA Removal section to Quality Control

--- scripts/cellbender.sh
+++ scripts/cellbender.sh
@@ -2,20 +2,16 @@
 #BSUB -J cellbender             # LSF job name
 #BSUB -o cellbender%J.out      # Name of the job output file 
 #BSUB -e cellbender.%J.error    # Name of the job error file
-#BSUB -n 5                 # Request cores
-#BSUB -M 200000              # JMEM=MB of memory for this job 
-#BSUB -R "rusage[mem=200000] span[hosts=1]"    # Make sure the job's assigned node has enough memory and that all cores are on the same worker node
-##BSUB -q gpu                 # Request GPU
-#BSUB -notify done         # Whenever the job finishes, email
-#BSUB -u chena23@seas.upenn.edu # User's email
-# #BSUB -m node123              # Specify node
-
-# :%s/JNAME/NEW_JNAME/g
-# :%s/JMEM/NEW_JMEM/g
+## BSUB -n 2                 # Request cores
+## BSUB -M 50000              # JMEM=MB of memory for this job 
+#BSUB -q gpu                 # Request GPU
 
 source py311env/bin/activate
 
+module load cuda/11.8
+
 cellbender remove-background \
+    --cuda \
     --input camr_scrublet_batch_filtered.h5ad \
     --output camr_scrublet_batch_filtered_bender.h5
 
