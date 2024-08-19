49fd2f4f4fd6e87a08fd016e9756886fc3bef398
JeffreyMaurer <jeffrey.maurer.informatics@gmail.com>
2024-08-19 14:37:09 -0400

    Cleaning house

--- scripts/test_job.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/bash
- #BSUB -J my_test_job            # LSF job name
- #BSUB -o my_test_job.%J.out     # Name of the job output file 
- #BSUB -e my_test_job.%J.error   # Name of the job error file
-
- echo "this is a test"
- sleep 15
