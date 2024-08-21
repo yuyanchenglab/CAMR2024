d04028b9557c64a3a089e93cb4d4d5953bd87aa5
JeffreyMaurer <jeffrey.maurer.informatics@gmail.com>
2024-08-21 09:13:24 -0400

    Fixed output naming to match other scripts.

--- scripts/make_raw_mean_expression.py
+++ scripts/make_raw_mean_expression.py
@@ -11,13 +11,13 @@ sc.settings.n_jobs = -1
 adata = ad.read_h5ad('data/camr_scrublet_batch_filtered.h5ad')
 genes = adata.var["feature_name"].astype(str).tolist()
 
-fmajorname = 'data/raw_majorclass_meanExpression.txt'
+fmajorname = 'data/raw_meanExpression_majorclass.txt'
 if not os.path.isfile(fname):
   raw_feature_expression_pd = pd.DataFrame(adata.raw.X.toarray(), columns = genes)
   raw_feature_expression_pd["majorclass"] = adata.obs["majorclass"].tolist()
   raw_feature_expression_pd.groupby("majorclass").agg("mean").to_csv(fmajorname, sep = '\t')
 
-fminorname = 'data/raw_subtype_mean_variable_genes.txt'
+fminorname = 'data/raw_meanExpression_minorclass.txt'
 if not os.path.isfile(fminorname):
   raw_feature_expression_pd = pd.DataFrame(adata.raw.X.toarray(), columns = genes)
   raw_feature_expression_pd["author_cell_type"] = adata.obs["author_cell_type"].tolist()
