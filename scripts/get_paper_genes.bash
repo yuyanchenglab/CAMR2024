14447dbc5673d6986e8568bf7f6787d92fbd8aa9
JeffreyMaurer <jeffrey.maurer.informatics@gmail.com>
2024-08-21 12:28:27 -0400

    script updated with new paths/names

--- scripts/get_paper_genes.bash
+++ scripts/get_paper_genes.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "" > mentioned_genes.txt
+cd /project/hipaa_ycheng11lab/atlas/CAMR2024/
+
+rm -f data/mentioned_genes.txt
 
 while read gene; do
-  grep -o -m 1 $gene CAMR_paper_text.txt >> mentioned_genes.txt
-done < CAMR_genes.csv
+  grep -o -m 1 $gene data/CAMR_paper_text.txt >> data/mentioned_genes.txt
+done < data/1_genes.txt
